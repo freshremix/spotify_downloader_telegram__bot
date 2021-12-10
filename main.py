c432c1e602ba343de51315a547419c6f2e34e9f8
freshremix <90133873+freshremix@users.noreply.github.com>
2021-12-11 01:36:05 +0330

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import os
 if 'BOT_TOKEN' in os.environ:
     token = os.environ.get('BOT_TOKEN')
 else:
-    token = 'token bot'
+    token = '5080287435:AAE3RqA5ZKTmjiCcKhnNNXWq2nolY-bjkiE'
 
 bot = telepot.Bot(token)
 
